1fbd323d6cb2fca7aa1205851bd151b4a1b098a6
shreya231008 <shreyachauhan2308@gmail.com>
2021-05-04 17:15:30 +0530

    Update orders.php

--- project/orders.php
+++ project/orders.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
-if($_SESSION['username']==session_id())
-	{
-/* 
 $connect = mysqli_connect("localhost","root","","cart_system");
 
-$sql = "SELECT * FROM orders INNER JOIN registration1 ON orders.name = registration1.username ";
+$sql = "SELECT * FROM orders INNER JOIN registration1 ON orders.name = registration1.username";
 
-$result = mysqli_query($connect,$sql); */
+$result = mysqli_query($connect,$sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,28 +16,80 @@ $result = mysqli_query($connect,$sql); */
             <title> CART SYSTEM</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.0/css/all.min.css" integrity="sha512-gRH0EcIcYBFkQTnbpO8k0WlsD20x5VzjhOA1Og8+ZUAhcMUCvd+APD35FJw3GzHAP3e+mP28YcDJxVr745loHw==" crossorigin="anonymous" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">  
-            <link rel="stylesheet" href="styles1.css">
+            <link rel="stylesheet" href="styles.css">
+     <style>
+     table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+     </style>
     </head>
     <body>
-    <?php
-    echo $_SESSION['username'];
-    ?>
-        <div>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Orders</th>
-                </tr>
+   
+ <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="index.php">FOODNEST</a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link active" href="index.php">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Categories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="checkout.php">Checkout</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cart.php"><i class="fas
+         fa-shopping-cart"></i> <span id="cart-item" class="badge badge-danger"> </span> </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+        
+        <div style="overflow-x:auto;">
+            <table style='font-family:"Courier New", Courier, monospace; font-size:120%'>
+            <th> CUSTOMER NAME</th>
+                       <th> CART ITEMS</th>
+                       <th> ADDRESS</th>
+                       <th>PMODE</th>               
                     <?php
                     if(mysqli_num_rows($result)>0)
                     {
                         while($row = mysqli_fetch_array($result))
                         {
                     ?>
-                        <tr>
-                        <td><?php echo $row["username"];?></td>
-                        <td><?php echo $row["products"];?></td>
-                        </tr>
+                    <?php
+                    $str = $row["username"];
+                    $fruits_ar=explode(" ",$str);
+                    $str1 = $row["products"];
+                    $fruits_ar1=explode(" ",$str1);?>
+                   
+                      
+                    <tr><td><?php $arrlength=count($fruits_ar1);
+                    echo($str)?></td>
+                         <td><?php echo($str1)?></td>
+ <td><?php echo($row["address"]) ?></td>
+<td><?php echo($row["pmode"]) ?></td>
+                    </tr>
+                   
                     <?php
                         }
                     }
