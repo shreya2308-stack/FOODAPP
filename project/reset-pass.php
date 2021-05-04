35fae4e84bfd0dbbce426dc643080cbf869506ab
Shrihari <65852304+shrreeeehari@users.noreply.github.com>
2021-05-05 05:12:17 +0530

    Update reset-pass.php

--- project/reset-pass.php
+++ project/reset-pass.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>Reset Password</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
     </head>
     <body>
 
